b19d4746f62ac051642d4a22e0d4a74b066b4098
Yash Vermani <yash25vermani@gmail.com>
2018-01-01 02:20:55 +0530

    settings.py

--- yashblog/settings.py
+++ yashblog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '+)kdn7oco0bayn7lf2pr_+4ox(kn)vh6*ouuyo*nfybqu9@wwt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['YashVermani.pythonanywhere.com']
 
